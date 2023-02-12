f76406429388dd285d6bf08f8cfd1257d7d7cfaa
Clovis Carmezini <cloviscarmezini14@gmail.com>
2023-02-12 17:59:21 -0300

    test: ensure Login navigates to signup correctly

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -6,6 +6,9 @@ import { faker } from '@faker-js/faker'
 import { InvalidCredentialsError } from '@/domain/errors'
 import 'jest-localstorage-mock'
 
+import { Router } from 'react-router-dom'
+import { createMemoryHistory } from 'history'
+
 type SutTypes = {
   sut: RenderResult
   authenticationSpy: AuthenticationSpy
@@ -15,13 +18,19 @@ type SutParams = {
   validationError: string
 }
 
+const history = createMemoryHistory()
+
 const makeSut = (params?: SutParams): SutTypes => {
   const validationStub = new ValidationStub()
   const authenticationSpy = new AuthenticationSpy()
 
   validationStub.errorMessage = params?.validationError ?? ''
 
-  const sut = render(<Login validation={validationStub} authentication={authenticationSpy} />)
+  const sut = render(
+    <Router location={history.location} navigator={history}>
+      <Login validation={validationStub} authentication={authenticationSpy} />
+    </Router>
+  )
 
   return {
     sut,
@@ -216,5 +225,14 @@ describe('Login Component', () => {
     })
 
     expect(localStorageSpy).toHaveBeenCalledWith('accessToken', authenticationSpy.account.accessToken)
+  })
+
+  test('Should go to signup page', async () => {
+    const { sut } = makeSut()
+    const register = sut.getByTestId('register')
+
+    fireEvent.click(register)
+
+    expect(history.location.pathname).toBe('/signup')
   })
 })
